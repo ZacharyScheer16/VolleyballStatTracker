bde45d828f26910db48edb813a4947d24e6d4f29
Zachary Scheer <zscheer00@gmail.com>
2025-10-09 22:16:37 -0700

    Dependencies s

---
+++ src/test/java/com/zacharyscheer/volleyballstattracker/VolleyBallStatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacharyscheer.volleyballstattracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleyBallStatTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
