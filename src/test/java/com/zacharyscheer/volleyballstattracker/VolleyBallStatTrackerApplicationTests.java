b05a38391a54863feb891299103a73790465ac97
Zachary Scheer <zscheer00@gmail.com>
2025-10-17 16:07:52 -0700

    adjusted again

--- src/test/java/com/zacharyscheer/volleyballstattracker/VolleyBallStatTrackerApplicationTests.java
+++ src/test/java/com/zacharyscheer/volleyballstattracker/VolleyBallStatTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.zacharyscheer.volleyballstattracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // <-- ADD THIS LINE
 class VolleyBallStatTrackerApplicationTests {
 
     @Test
