6c1c39440d1405a765a7ad36948b4fa5f797b91e
Zachary Scheer <zscheer00@gmail.com>
2025-11-07 23:22:02 -0700

    Match Service interface

---
+++ src/main/java/com/zacharyscheer/volleyballstattracker/repository/MatchRepository.java
@@ -0,0 +1,15 @@
+package com.zacharyscheer.volleyballstattracker.repository;
+
+import com.zacharyscheer.volleyballstattracker.models.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MatchRepository extends JpaRepository<Match, Long> {
+
+    /**
+     * Finds all matches for a user, ordered by the date the match was played,
+     * from newest to oldest. This replaces the invalid 'MatchScore' order.
+     */
+    List<Match> findByUserIdOrderByMatchDateDesc(Integer userId);
+}
