439873cd61c5f096ddb42cf3ba8010414f04eeed
Zachary Scheer <zscheer00@gmail.com>
2025-12-22 11:19:44 -0700

    Setting attempts/error/assists controllers

--- src/main/java/com/zacharyscheer/volleyballstattracker/controller/StatController.java
+++ src/main/java/com/zacharyscheer/volleyballstattracker/controller/StatController.java
@@ -82,6 +82,7 @@ public class StatController {
         StatLine updatedStats = statLineService.recordServiceError(setId, playerId);
         return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
     }
+
     @PostMapping("/record/dig")
     public ResponseEntity<StatLineResponseDTO> recordDig(@RequestParam Long setId, @RequestParam Integer playerId) {
         StatLine updatedStats = statLineService.recordDig(setId, playerId);
@@ -93,14 +94,34 @@ public class StatController {
         StatLine updatedStats = statLineService.recordDigError(setId, playerId);
         return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
     }
+
     @PostMapping("/record/block")
     public ResponseEntity<StatLineResponseDTO> recordBlocked(@RequestParam Long setId, @RequestParam Integer playerId) {
-          StatLine updatedStats = statLineService.recordBlock(setId, playerId);
+        StatLine updatedStats = statLineService.recordBlock(setId, playerId);
         return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
     }
+
     @PostMapping("/record/block-error")
-    public ResponseEntity<StatLineResponseDTO> recordBlockError(@RequestParam Long setId, @RequestParam Integer playerId){
+    public ResponseEntity<StatLineResponseDTO> recordBlockError(@RequestParam Long setId, @RequestParam Integer playerId) {
         StatLine updatedStats = statLineService.recordBlockError(setId, playerId);
         return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
     }
+
+    @PostMapping("/record/set")
+    public ResponseEntity<StatLineResponseDTO> recordSetAttempt(@RequestParam Long setId, @RequestParam Integer playerId) {
+        StatLine updatedStats = statLineService.recordSetAttempt(setId, playerId);
+        return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
     }
+
+    @PostMapping("/record/set-error")
+    public ResponseEntity<StatLineResponseDTO> recordSetError(@RequestParam Long setId, @RequestParam Integer playerId) {
+        StatLine updatedStats = statLineService.recordSetError(setId, playerId);
+        return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
+    }
+
+    @PostMapping("/record/assist")
+    public ResponseEntity<StatLineResponseDTO> recordAssist(@RequestParam Long setId, @RequestParam Integer playerId) {
+        StatLine updatedStats = statLineService.recordAssist(setId, playerId);
+        return ResponseEntity.ok(statLineMapper.toDto(updatedStats));
+    }
+}
